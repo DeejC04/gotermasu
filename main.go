b230771a2b94af9a23cc622e864066115617535d
Deej Cabral <deejc204@gmail.com>
2024-08-19 02:57:40 -0700

    this has major errors, just pushing to transfer to my pc :3

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
-	package main
+package main
 
-	import (
-		// "encoding/json"
-		"encoding/json"
-		"fmt"
-		"io"
-		"log"
-		"net/http"
-		// "flag"
-	)
+import (
+	// "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 	type Course []struct {
 		Subject          string `json:"SUBJECT"`
@@ -44,6 +45,28 @@
 	}
 
 	func main() {
+        // search := flag.String("search", "", "what are you searching for (terms, courses, class sections, subjects)")
+		// flag.Parse()
+
+
+		searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+		searchCourses := searchCmd.String("course", "", "search course")
+
+
+		switch os.Args[1] {
+		case "search":
+			searchCmd.Parse(os.Args[2:])
+			
+		default:
+			fmt.Println("unknown command")
+			os.Exit(1)
+		}
+
+		var refine string
+
+		queryString := ""
+
+
 		// will be used to make requests with headers (doesn't seem like
 		// default http.Get() command can use headers
 		client := &http.Client{}
@@ -51,7 +74,7 @@
 
 		// creating the request itself, afaik it doesn't actually send it yet
 		// since I'm literally just creating a new request
-		req, err := http.NewRequest("GET", (url + "/search/courses?refine=Y&catalogNbr=205&course_id=104182&subject=CSE&term=2247"), nil)
+		req, err := http.NewRequest("GET", (url + ), nil)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -80,5 +103,8 @@
 		// by default, response is a byte slice (research it), so it must be converted
 		// to a string
 		fmt.Println(course[0].Subject)
-
 	}
+
+
+	// Example cli for course search:
+	// courseCli search -course CSE 110
